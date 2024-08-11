1c030503168ebffd1dfe030218e896905564927a
Pekskumar <79084617+Pekskumar@users.noreply.github.com>
2024-08-11 23:21:33 +0530

    projectdata changes

--- src/Data/Projectdata.js
+++ src/Data/Projectdata.js
@@ -34,6 +34,95 @@ const Projectdata = [
             'Chart.js'
         ]
     },
+    {
+        title: 'Expense Tracker',
+        subtitle: 'et',
+        detail: "💻 A fully responsive Expense Tracker app that I developed, featuring user authentication, dynamic dashboards, transaction filters, and a built-in to-do list. It includes full CRUD operations and is built with React, RTK, and ApexCharts, providing real-time insights with secure data storage in MongoDB, and deployment on Render.com. 📱",
+        sourcelink: 'https://github.com/Pekskumar/expense-tracker',
+        livelink: 'https://pekskumar.github.io/expense-tracker',
+        technology: [
+            'react',
+            'RTK',
+            'React Bootstrap',
+            'css',
+            'html',
+            'node',
+            'MongoDB',
+            'render server',
+            'Apexcharts']
+    },
+    {
+        title: 'Gmail Clone',
+        subtitle: 'gr',
+        detail: "💻 A meticulously designed Gmail clone that mirrors the Google Mail interface, offering a seamless and intuitive email experience. Utilizing the react-oauth/google package, this project fetches real-time email data, ensuring users have up-to-date information at their fingertips.",
+        sourcelink: '',
+        livelink: 'https://pekskumar.github.io/gmail-react',
+        technology: [
+            'react',
+            'RTK',
+            'React Bootstrap',
+            'css',
+            'html',
+        ]
+    },
+    {
+        title: 'Microsoft Clone',
+        subtitle: 'mr',
+        detail: "💻 A meticulously crafted Microsoft clone that faithfully replicates the Microsoft interface, delivering a smooth and familiar user experience. This project utilizes the @azure/msal-react package to fetch real-time email data from Microsoft, ensuring users have access to the latest information effortlessly.",
+        sourcelink: '',
+        livelink: 'https://pekskumar.github.io/microsoft-react',
+        technology: [
+            'react',
+            'RTK',
+            'React Bootstrap',
+            'css',
+            'html',
+        ]
+    },
+    {
+        title: 'Google Analytic',
+        subtitle: 'ga-4',
+        detail: "💻 A comprehensive and fully mobile-responsive Google Analytic project that aggregates and displays extensive user traffic data from various live websites. Featuring a Node.js backend API deployed on Render.com, this project gathers data on user locations, device types, new vs. returning visitors, event counts, and more. The data is visualized through detailed charts on a React-powered interface, offering insights into user behavior, landing pages, top referring sites, and screen resolutions, all seamlessly grouped and presented for easy analysis. 📱",
+        sourcelink: '',
+        livelink: 'https://pekskumar.github.io/google-analytic',
+        technology: [
+            'react',
+            'RTK',
+            'React Bootstrap',
+            'css',
+            'html',
+            'node',
+            'render server',
+            'Chart.js'
+        ]
+    },
+    {
+        title: 'Google Search Engine',
+        subtitle: 'gse',
+        detail: "💻 A dynamic and fully responsive Google Search Engine project that leverages the Google Custom Search API to deliver precise and efficient search results. Designed with a clean and intuitive interface, this project provides a seamless search experience, mirroring the functionality and ease of use found in Google's own search platform. 📱",
+        sourcelink: '',
+        livelink: 'https://pekskumar.github.io/google-search-engine',
+        technology: [
+            'react',
+            'RTK',
+            'React Bootstrap',
+            'css',
+            'html',
+        ]
+    },
+    {
+        title: 'Task Board',
+        subtitle: 'taskboard',
+        detail: "💻 A sleek Task Board application that leverages local storage for task management. This project features custom drag-and-drop functionality, allowing users to effortlessly move tasks between stages like 'To Do,' 'In Progress,' 'In Review,' and 'Done.' With robust CRUD operations for modifying and deleting tasks, it provides a streamlined and interactive experience for managing workflows.",
+        sourcelink: '',
+        livelink: 'https://pekskumar.github.io/TaskBoard',
+        technology: [
+            'react',
+            'RTK',
+            'React Bootstrap',
+            'css',
+            'html']
+    },
 
 ]
 
