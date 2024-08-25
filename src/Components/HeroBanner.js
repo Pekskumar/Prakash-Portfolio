f0282a4c5ad58b252b250c096898a9ac1c8b4c53
Pekskumar <79084617+Pekskumar@users.noreply.github.com>
2024-08-25 20:33:25 +0530

    hero & about content done

--- src/Components/HeroBanner.js
+++ src/Components/HeroBanner.js
@@ -11,9 +11,10 @@ const HeroBanner = () => {
         <Row>
           <Col md={6}>
             <div className=''>
-              <h1 className='mb-3'>Prakash Parmar</h1>
-              <h2 className='mb-3'>React Js Developer</h2>
-              <div className='hero-detail mb-3'>Welcome to my digital world! I'm on a mission to craft captivating and intuitive digital experiences. As a passionate fullstack developer, I blend cutting-edge technology with creativity to bring ideas to life.</div>
+              <h1 className='mb-3'>Hey 👋</h1>
+              <h1 className='mb-3'>I'm Prakash Parmar</h1>
+              <h3 className='mb-3'>React Js Developer</h3>
+              <div className='hero-detail mb-3'>Welcome to my personal website! Here you can find more about me, my work, and how to contact me. Feel free to explore all the sections to learn more.</div>
               <div className='hero-btn project-btn d-flex '>  <Button variant="main-btn me-2 ">Get Resume <span className='ms-2'> <DownloadIcon /></span></Button><Button variant="main-btn  text-center">Hire me</Button></div>
             </div>
           </Col>
